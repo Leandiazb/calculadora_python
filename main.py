d5f42a495dea6e81f6c42686092e1163dbe87f57
Leandro Diaz <leandiazb@gmail.com>
2026-09-21 20:40:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from operaciones import sumar, restar
+
+resultado_suma = sumar(5, 3)
+resultado_resta = restar(5, 3)
+
+print ("el resultado de la suma es: ", resultado_suma)
+print ("el resultado de la resta es: ", resultado_resta)
