fb8818112a9bd1c66d6ebf73ae580dd227ecd0ed
Leandro Diaz <leandiazb@gmail.com>
2026-09-21 22:03:15 -0300

    se agrego la funcion division

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from operaciones import sumar, restar, multiplicar
+from operaciones import sumar, restar, multiplicar, dividir
 
 resultado_suma = sumar(5, 3)
 resultado_resta = restar(5, 3)
+resultado_division = dividir(5, 3)
 
 print ("el resultado de la suma es: ", resultado_suma)
 print ("el resultado de la resta es: ", resultado_resta)
 print ("el resultado de la multiplicacion es: ", multiplicar(5, 3))
+print ("el resultado de la division es: ", resultado_division)
