ec406db445f19eafcf6f71202fd947a495f31728
Leandro Diaz <leandiazb@gmail.com>
2026-09-21 21:37:25 -0300

    Se agrego la multiplicación

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from operaciones import sumar, restar
+from operaciones import sumar, restar, multiplicar
 
 resultado_suma = sumar(5, 3)
 resultado_resta = restar(5, 3)
 
 print ("el resultado de la suma es: ", resultado_suma)
 print ("el resultado de la resta es: ", resultado_resta)
+print ("el resultado de la multiplicacion es: ", multiplicar(5, 3))
